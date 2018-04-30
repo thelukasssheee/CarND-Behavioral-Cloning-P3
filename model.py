6927cc1fb2274876578157118fcd3e80ea431795
TheLukasssHeee <lukasss@lukassss-iMac.fritz.box>
2018-05-01 00:06:31 +0200

    v2 - Udacity data + 1 round track 1

--- model.py
+++ model.py
@@ -1,39 +1,54 @@
 import csv
 import cv2
 import numpy as np
+import ntpath
+import time
+from tqdm import tqdm
 
-### Open CSV file of Udacity test image dataset
+
+### List of available datasets
+datasets = ['data/Udacity/driving_log.csv', \
+            'data/T1_Regular/driving_log.csv']
+
+print('\nDatasets used: "',datasets,'"', sep='')
 lines = []
-with open('data/Udacity/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
-lines_header = lines[0]
-lines.remove(lines[0])
-# print(lines[0])
-# print(lines_header)
-# lines.remove(lines[0])
-# print(lines[0])
-# quit()
+
+### Open CSV files of provided datasets
+for csvpath in datasets:
+    with open(csvpath) as csvfile:
+    # with open('data/Udacity/driving_log.csv') as csvfile:
+        print('  Processing file "',csvpath,'"... ',sep='',end='')
+        reader = csv.reader(csvfile, skipinitialspace=True)
+        j = 0
+        for line in reader:
+            if j == 0:
+                lines_header = line
+                j = j + 1
+                continue
+            else:
+                line[0] = ntpath.split(csvpath)[0]+'/IMG/'+ntpath.split(line[0])[-1]
+                line[1] = ntpath.split(csvpath)[0]+'/IMG/'+ntpath.split(line[1])[-1]
+                line[2] = ntpath.split(csvpath)[0]+'/IMG/'+ntpath.split(line[2])[-1]
+                lines.append(line)
+        print('done! Total amount of images sums up to',len(lines))
 
 ### Read images
 images = []
 measurements = []
-print("Begin reading of images...", end=" ")
-for line in lines:
+print("\nBegin reading of images:",)
+for line in tqdm(lines, total=len(lines)):
     source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = 'data/Udacity/IMG/' + filename
-    image = cv2.imread(current_path)
+    # filename = source_path.split('/')[-1]
+    # current_path = 'data/Udacity/IMG/' + filename
+    image = cv2.imread(source_path)
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
-print("done!")
-print("Steering angle information:", len(measurements))
-print("Dimensions of image container:", np.shape(images))
-#quit()
+print("Amount of steering angle information: ", len(measurements))
+print("Dimensions of image container:        ", np.shape(images))
 
 ### Keras Neural Network
+print("\n Starting Keras instance")
 X_train = np.array(images)
 y_train = np.array(measurements)
 
@@ -45,6 +60,6 @@ model.add(Flatten(input_shape=(160,320,3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
 
 model.save('model.h5')
